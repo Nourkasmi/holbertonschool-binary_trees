1854c0220d8202686ac3161f432fb34440373e01
NourKasmi <nourkasmi@gmail.com>
2024-12-11 14:06:26 +0100

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,14 +6,17 @@
  *
  * Return: The height of the tree. If tree is NULL, return 0.
  */
-static size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_height, right_height;
-if (tree == NULL)
-return (0);
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
-return (left_height > right_height ? left_height + 1 : right_height + 1);
+    size_t left_height, right_height;
+
+    if (tree == NULL)
+        return (0);
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    return (left_height > right_height ? left_height + 1 : right_height + 1);
 }
 
 /**
@@ -24,7 +27,8 @@ return (left_height > right_height ? left_height + 1 : right_height + 1);
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-if (tree == NULL)
-return (0);
-return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+    if (tree == NULL)
+        return (0);
+    return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
+
