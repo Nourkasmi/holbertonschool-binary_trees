66f032d0a3d8ed452b80cbe9253754249653f17b
NourKasmi <nourkasmi@gmail.com>
2024-12-11 10:14:03 +0100

    satsifing betty

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Traverses a binary tree using pre-order traversal
+ * binary_tree_inorder - Traverses a binary tree using in-order traversal
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
  *
- * Description: Pre-order traversal visits nodes in the following order:
+ * Description: in-order traversal visits nodes in the following order:
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
