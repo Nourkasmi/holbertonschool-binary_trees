019a30b30a0719fd284bc20bbeef95095fff6f42
NourKasmi <nourkasmi@gmail.com>
2024-12-11 17:17:43 +0100

    is it full

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,8 +9,6 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 size_t left_height, right_height;
-size_t left_height, right_height;
-size_t left_height, right_height;
 if (tree == NULL)
 return (0);
 left_height = binary_tree_height(tree->left);
