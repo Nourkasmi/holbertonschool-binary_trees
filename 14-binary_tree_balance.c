c63eb292d61b877e9d638f3d300940cf45c467f5
NourKasmi <nourkasmi@gmail.com>
2024-12-11 17:16:01 +0100

    is it full

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,15 +8,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
-
-    if (tree == NULL)
-        return (0);
-
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-
-    return (left_height > right_height ? left_height + 1 : right_height + 1);
+size_t left_height, right_height;
+size_t left_height, right_height;
+size_t left_height, right_height;
+if (tree == NULL)
+return (0);
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
+return (left_height > right_height ? left_height + 1 : right_height + 1);
 }
 
 /**
@@ -27,8 +26,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+if (tree == NULL)
+return (0);
+return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
-
