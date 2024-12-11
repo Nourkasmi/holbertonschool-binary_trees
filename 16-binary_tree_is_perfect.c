938d9f358a95454e97d7f8a07cf116c970176032
NourKasmi <nourkasmi@gmail.com>
2024-12-11 17:30:11 +0100

    is it perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,10 +8,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+size_t left_height, right_height;
 if (!tree)
 return (0);
-return (1 + ((binary_tree_height(tree->left) > binary_tree_height(tree->right)) ?
-binary_tree_height(tree->left) : binary_tree_height(tree->right)));
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
+return (1 + (left_height > right_height ? left_height : right_height));
 }
 
 /**
